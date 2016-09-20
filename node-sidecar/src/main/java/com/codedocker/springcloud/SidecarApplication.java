5980afaf474d2789a11f92227e52965c2f3575b8
marshalYuan <marshal_o@163.com>
2016-09-20 20:26:03 +0800

    finish eureka server

---
+++ node-sidecar/src/main/java/com/codedocker/springcloud/SidecarApplication.java
@@ -0,0 +1,14 @@
+package com.codedocker.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableSidecar
+public class SidecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidecarApplication.class, args);
+	}
+}
