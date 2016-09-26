bf3d348d1920d23174ddf9d0b26d9b7bbbd1eb2b
marshal yuan <marshal yuan>
2016-09-27 04:18:00 +0800

    change config server

--- config-server/src/main/java/com/codedocker/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/codedocker/springcloud/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.codedocker.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
